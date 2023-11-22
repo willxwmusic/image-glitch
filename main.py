fb7980af8a594435952e9795b39b3418b338dc3f
willxw <willowgoodwin2006@gmail.com>
2023-11-22 11:15:47 +0000

    Cleaned up button code

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def clampSize(n, bound):
     else: 
         return n 
 
-def colourbend(redOffset,greenOffset,blueOffset,alphaOffset, randomiseOffset, randomnessAmount):
+def colourbend(redOffset,greenOffset,blueOffset,alphaOffset,randomiseOffset,randomnessAmount):
 
     if randomiseOffset == True:
 
@@ -76,29 +76,32 @@ def open_image():
 
 def apply_processing():
     pixelShift(16,imgwidth/2)
-    colourbend(red_offset_slider.get(),green_offset_slider.get(),blue_offset_slider.get(),alpha_offset_slider.get(),alt_colour_bend,0.2)
+    colourbend(red_offset_slider.get(),green_offset_slider.get(),blue_offset_slider.get(),alpha_offset_slider.get(),alt_colour_bend,randomness_offset_slider.get())
     applied_notification = Label(root, text="Processing Applied!")
     applied_notification.place(x=150, y=500)
 
 def save_image():
     save_directory = filedialog.asksaveasfilename(initialdir="file_path", title="Hola", filetypes=(("TIFF","*.tiff"),("All files","*.*")))
     tiff.imwrite(save_directory, data, photometric='rgb')
+    
+def add_button(button_text,button_command,button_x,button_y):
+    new_button = Button(root, text =button_text, command = button_command)
+    new_button.place(x=button_x,y=button_y)
 
-def add_slider(new_slider,slider_text, slider_x, slider_y):
+def add_slider(slider_text, slider_min, slider_max, slider_x, slider_y):
     new_slider_label = Label(root, text=slider_text)
     new_slider_label.place(x=slider_x,y=slider_y)
+    new_slider = ttk.Scale(root, from_=slider_min, to=slider_max, orient=HORIZONTAL)
     new_slider.place(x=(slider_x+150), y=slider_y)
+    return new_slider
 
 root = Tk()
 root['background']='#222222'
 root.minsize(width="400", height="600")
-open_button = Button(root, text ="Open", command = open_image)
-apply_button = Button(root, text ="Apply", command = apply_processing)
-save_button = Button(root, text ="Save", command = save_image)
 
-open_button.place(x=0,y=0)
-apply_button.place(x=100,y=0)
-save_button.place(x=200, y=0)
+open_button = add_button("Open",open_image,0,0)
+apply_button = add_button("Apply",open_image,100,0)
+save_button = add_button("Save",open_image,200,0)
 
 do_pixel_shift = IntVar
 pixel_shift_checkbox = Checkbutton(root, text='Pixel Shifting',variable=do_pixel_shift, onvalue=True, offvalue=False)
@@ -112,31 +115,17 @@ alt_colour_bend_checkbox = Checkbutton(root, text='Alt. Colour Bending',variable
 colour_bend_checkbox.place(x=0,y=50)
 alt_colour_bend_checkbox.place(x=0,y=75)
 
-red_offset_slider = ttk.Scale(root, from_=0, to=255, orient=HORIZONTAL)
-add_slider(red_offset_slider,'Red Offset',0,100)
+red_offset_slider = add_slider('Red Offset',0,255,10,100)
 
-green_offset_slider = ttk.Scale(root, from_=0, to=255, orient=HORIZONTAL)
-add_slider(red_offset_slider,'Green Offset',0,100)
+green_offset_slider = add_slider('Green Offset',0,255,10,150)
 
-blue_offset_slider = ttk.Scale(root, from_=0, to=255, orient=HORIZONTAL)
-add_slider(red_offset_slider,'Blue Offset',0,100)
+blue_offset_slider = add_slider('Blue Offset',0,255,10,200)
 
-alpha_offset_slider = ttk.Scale(root, from_=0, to=1, orient=HORIZONTAL)
-add_slider(red_offset_slider,'Alpha Offset',0,100)
+alpha_offset_slider = add_slider('Alpha Offset',0,1,10,250)
 
-randomness_slider_label = Label(root, text="Randomness")
-randomness_slider_label.place(x=0,y=315)
-randomness_slider = ttk.Scale(root, from_=0, to=1, orient=HORIZONTAL)
-randomness_slider.place(x=150, y=315)
+radnomness_offset_slider = add_slider('Randomness Offset',0,1,10,300)
 
 pixel_shift_checkbox.place(x=0,y=375)
-
-green_offset_slider_label = Label(root, text="Pixel Shift Amount")
-green_offset_slider_label.place(x=0,y=415)
-green_offset_slider = ttk.Scale(root, from_=0, to=4, orient=HORIZONTAL)
-green_offset_slider.place(x=150 , y=415)
-
-
 
 root.mainloop()
 
