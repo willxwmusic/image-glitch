dcc1b029bad2e4f81f4cb2dedf6ffe0eb2b118cd
willxw <willowgoodwin2006@gmail.com>
2023-11-29 10:24:45 +0000

    Create imagekenesis.py

---
+++ imagekenesis.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from tkinter import ttk
+
+class window(tk.Tk):
+    def __init__(self, *args, **kwargs):
+
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        # Adding a title to the window
+        self.wm_title("Test Application")
+
+        # creating a frame and assigning it to container
+        container = tk.Frame(self, height=400, width=600)
+
+        # specifying the region where the frame is packed in root
+        container.pack(side="top", fill="both", expand=True)
+
+        # configuring the location of the container using grid
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        # We will now create a dictionary of frames
+        self.frames = {}
+
+        # we'll create the frames themselves later but let's add the components to the dictionary.
+        for F in (Startup, Processing, Completion):
+            frame = F(container, self)
+
+            # the windows class acts as the root window for the frames.
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        # Using a method to switch frames
+        self.show_frame(MainPage)
