7e90b10272dbae5cf9b5696c46be41a1918c0851
willxw <willowgoodwin2006@gmail.com>
2023-11-30 12:39:07 +0000

    renamed files

--- imagekenesis.py
+++ imagekenesis.py
@@ -1,88 +1,164 @@
-import tkinter as tk
+import tifffile as tiff
+import imageio.v3 as iio
+
+import random
+import numpy
+import shutil
+
+from pathlib import Path
+from tkinter import *
+from tkinter import filedialog
 from tkinter import ttk
+from PIL import Image
 
-class window(tk.Tk):
-    def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
-        self.wm_title("Imagekenesis")
-        self.geometry('640x360')
-        self.minsize(640, 360)
-        container = tk.Frame(self, height=640, width=360)
-        container.pack(side="top", fill="both", expand=True)
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
-        self.frames = {}
-        for F in (Startup, Processing, Completion):
-            frame = F(container, self)
-            self.frames[F] = frame
-            frame.grid(row=0, column=0, sticky="nsew")
-        self.display_frame(Startup)
+def clampSize(n, bound): 
+    if n < 0: 
+        return 0
+    elif n > bound: 
+        return bound
+    else: 
+        return n 
 
-    def display_frame(self, cont):
-        frame = self.frames[cont]
-        frame.tkraise()
+def colourbend(data,redOffset,greenOffset,blueOffset,altColourBend,randomnessAmount):
+    if altColourBend == 1:
+        for x in range(0, imgwidth):
+            redRandomness = random.uniform(-randomnessAmount,randomnessAmount)
+            greenRandomness = random.uniform(-randomnessAmount,randomnessAmount)
+            blueRandomness = random.uniform(-randomnessAmount,randomnessAmount)
+            for y in range(0, imgheight):
+                data[y][x][0] -= redOffset * redRandomness
+                data[y][x][1] -= greenOffset * greenRandomness
+                data[y][x][2] -= blueOffset * blueRandomness
+    else:
+        for x in range(0, imgwidth):
+            for y in range(0, imgheight):
+                redRandomness = random.uniform(-randomnessAmount,randomnessAmount) + 1
+                greenRandomness = random.uniform(-randomnessAmount,randomnessAmount) + 1
+                blueRandomness = random.uniform(-randomnessAmount,randomnessAmount) + 1
+                data[y][x][0] -= redOffset * redRandomness
+                data[y][x][1] -= greenOffset * greenRandomness
+                data[y][x][2] -= blueOffset * blueRandomness
 
-class Startup(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        inner_frame = tk.Frame(self, bg='#121212')
-        inner_frame.pack(fill='both', expand=True,)
-        center_frame = tk.Frame(inner_frame, bg='#202020')
-        center_frame.pack(side="left", fill="both", expand=1, padx=10, pady=10)
+def pixelShift(data,frequency_exponent,horizontalOffset):
+    frequency = 2^round(frequency_exponent)
+    for i in range(0,(frequency)):
+        offset = random.randint(round(horizontalOffset/2),horizontalOffset)
+        for y in range(int(i*imgheight/frequency),int((i+1)*imgheight/frequency)):
+            for x in range(0,imgwidth):
+                if x-offset > 0:
+                    data[y][x] = original[y][x-offset]
+                else:
+                    data[y][x] = original[y][(imgwidth-1) + (x-offset)]
 
-        title = tk.Label(
-            center_frame,
-            text="Welcome to Imagekenesis!",
-            bg='#202020',
-            fg='white',
-            font=("Arial", 25) 
-        )
-        
-        subtitle = tk.Label(
-            center_frame,
-            text="by willxw",
-            bg='#202020',
-            fg='white'
-        )
+def open_image():
+    global file_path
+    file_path = filedialog.askopenfilename(initialdir="file_path", title="Hola", filetypes=(("All files","*.*"),("png","*.png")))
+    global original 
+    if file_path.endswith(".png"):
+        original = iio.imread(file_path)
+    else:
+        original = tiff.imread(file_path)
 
-        title.pack(padx=20, pady=20)
-        subtitle.pack(padx=10, pady=10)
+    global image
+    image = PhotoImage(file=file_path)
+    global imgwidth
+    imgwidth = original.shape[1]
+    global imgheight
+    imgheight = original.shape[0]
 
-        switch_window_button = tk.Button(
-            center_frame,
-            text="Open Image",
-            command=lambda: controller.display_frame(Processing),
-            highlightbackground='#202020'
-        )
+def apply_processing():
+    if file_path.endswith(".png"):
+        data = iio.imread(file_path)
+    else:
+        data = tiff.imread(file_path)
+    if do_pixel_shift.get() == 1:
+        pixelShift(data,pixel_shift_slider.get(),round(imgwidth/2))
+    if do_colour_bend.get() == 1:
+        colourbend(data,red_offset_slider.get(),green_offset_slider.get(),blue_offset_slider.get(),alt_colour_bend.get(),randomness_offset_slider.get())
+    if file_path.endswith(".png"):
+        iio.imwrite("./temp.png", data, photometric='rgb')
+        edited_image=PhotoImage(file="./temp.png").subsample(subasmple_ratio)
+        edited_image_display.config(image=edited_image)
+        edited_image_display.image = edited_image
 
-        switch_window_button.pack(padx=20, pady=20)
-
-
-
-class Processing(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        inner_frame = tk.Frame(self, bg='#121212')
-        inner_frame.pack(fill='both', expand=True,)
-
-        left_frame = tk.Frame(inner_frame, bg='#202020')
-        right_frame = tk.Frame(inner_frame, bg='#202020')
-
-        left_frame.pack(side="left", fill="both", expand=1, padx=10, pady=10)
-        right_frame.pack(side="right", fill="both", expand=1, padx=10, pady=10)
-
-
-class Completion(tk.Frame):
+def save_image():
+    save_directory = filedialog.asksaveasfilename(initialdir="file_path", title="Hola", filetypes=(("PNG","*.png"),("TIFF","*.tiff"),("All files","*.*")))
+    shutil.copyfile("./temp.png",save_directory)
     
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Completion Screen, we did it!")
-        label.pack(padx=10, pady=10)
-        switch_window_button = ttk.Button(
-            self, text="Return to menu", command=lambda: controller.show_frame(MainPage)
-        )
-        switch_window_button.pack(side="bottom", fill=tk.X)
+def add_button(button_text,button_command,button_row,button_column):
+    Button(tool_bar,text=button_text,command=button_command).grid(row=button_row,column=button_column,padx=5,pady=5)
 
-if __name__ == "__main__":
-    testObj = window()
-    testObj.mainloop()
+def add_slider(slider_window, slider_text, slider_min, slider_max, slider_row, slider_column):
+    new_slider_label = Label(slider_window, text=slider_text)
+    new_slider_label.grid(row=slider_row,column=slider_column)
+    new_slider = ttk.Scale(slider_window, from_=slider_min, to=slider_max, orient=HORIZONTAL)
+    new_slider.grid(row=(slider_row+1),column=slider_column)
+    return new_slider
+
+root = Tk()
+root.title("imagekenesis")
+root.geometry("1920x500")
+root.config(bg="#222222")
+
+left_frame = Frame(root, width=400, height=400, bg='grey')
+left_frame.grid(row=0, column=0, padx=10, pady=5)
+middle_frame = Frame(root, width=650, height=400, bg='grey')
+middle_frame.grid(row=0, column=1, padx=10, pady=5)
+right_frame = Frame(root, width=400, height=400, bg='grey')
+right_frame.grid(row=0, column=2, padx=10, pady=5)
+
+# Create frames and labels in left_frame
+Label(left_frame, text="Original Image").grid(row=0, column=0, padx=5, pady=5)
+
+open_image()
+
+# load image to be "edited"
+
+average_image_size = (imgheight+imgwidth) / 2
+subasmple_ratio = round(average_image_size / 400)
+original_image = image.subsample(subasmple_ratio * 2)
+edited_image = image.subsample(subasmple_ratio)
+
+original_image_display = Label(left_frame, image=original_image)
+original_image_display.grid(row=1, column=0, padx=5, pady=5)
+edited_image_display = Label(middle_frame, image=edited_image)
+edited_image_display.grid(row=0,column=0, padx=5, pady=5)
+
+# Create tool bar frame
+tool_bar = Frame(left_frame, width=180, height=185)
+tool_bar.grid(row=2, column=0, padx=5, pady=5)
+filter_bar = Frame(right_frame, width=180, height=185)
+filter_bar.grid(row=2, column=0, padx=5, pady=5)
+
+# File menu
+Label(tool_bar, text="File", relief=RAISED).grid(row=0, column=0, padx=5, pady=3, ipadx=10)
+open_button = add_button("Open",open_image,1,0)
+apply_button = add_button("Apply",apply_processing,2,0)
+save_button = add_button("Save",save_image,3,0)
+
+#Tools menu
+Label(tool_bar, text="Tools", relief=RAISED).grid(row=0, column=1, padx=5, pady=3, ipadx=10)
+
+#Filters menu
+Label(filter_bar, text="Colour", relief=RAISED).grid(row=0, column=0, padx=5, pady=3, ipadx=10)
+do_colour_bend = IntVar()
+colour_bend_checkbox = Checkbutton(filter_bar, text='Colour Bending',variable=do_colour_bend, onvalue=True, offvalue=False)
+colour_bend_checkbox.grid(row=1,column=0)
+alt_colour_bend = IntVar()
+alt_colour_bend_checkbox = Checkbutton(filter_bar, text='Alt. Colour Bending',variable=alt_colour_bend, onvalue=True, offvalue=False)
+alt_colour_bend_checkbox.grid(row=2,column=0)
+red_offset_slider = add_slider(filter_bar,'Red Offset',0,255,3,0)
+green_offset_slider = add_slider(filter_bar,'Green Offset',0,255,5,0)
+blue_offset_slider = add_slider(filter_bar,'Blue Offset',0,255,7,0)
+randomness_offset_slider = add_slider(filter_bar,'Randomness Offset',0,1,9,0)
+alpha_offset_slider = add_slider(filter_bar,'Alpha Offset',0,1,11,0)
+
+#Shifting Menu
+Label(filter_bar, text="Shifting", relief=RAISED).grid(row=0, column=1, padx=5, pady=3, ipadx=10)
+do_pixel_shift = IntVar()
+pixel_shift_checkbox = Checkbutton(filter_bar, text='Pixel Shifting',variable=do_pixel_shift, onvalue=True, offvalue=False)
+pixel_shift_checkbox.grid(row=1,column=1)
+pixel_shift_slider = add_slider(filter_bar,'Shift Offset',0,8,2,1)
+
+# File Menu
+root.mainloop()
---
+++ ui-update-prototype.py
@@ -0,0 +1,88 @@
+import tkinter as tk
+from tkinter import ttk
+
+class window(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.wm_title("Imagekenesis")
+        self.geometry('640x360')
+        self.minsize(640, 360)
+        container = tk.Frame(self, height=640, width=360)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+        self.frames = {}
+        for F in (Startup, Processing, Completion):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+        self.display_frame(Startup)
+
+    def display_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+class Startup(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        inner_frame = tk.Frame(self, bg='#121212')
+        inner_frame.pack(fill='both', expand=True,)
+        center_frame = tk.Frame(inner_frame, bg='#202020')
+        center_frame.pack(side="left", fill="both", expand=1, padx=10, pady=10)
+
+        title = tk.Label(
+            center_frame,
+            text="Welcome to Imagekenesis!",
+            bg='#202020',
+            fg='white',
+            font=("Arial", 25) 
+        )
+        
+        subtitle = tk.Label(
+            center_frame,
+            text="by willxw",
+            bg='#202020',
+            fg='white'
+        )
+
+        title.pack(padx=20, pady=20)
+        subtitle.pack(padx=10, pady=10)
+
+        switch_window_button = tk.Button(
+            center_frame,
+            text="Open Image",
+            command=lambda: controller.display_frame(Processing),
+            highlightbackground='#202020'
+        )
+
+        switch_window_button.pack(padx=20, pady=20)
+
+
+
+class Processing(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        inner_frame = tk.Frame(self, bg='#121212')
+        inner_frame.pack(fill='both', expand=True,)
+
+        left_frame = tk.Frame(inner_frame, bg='#202020')
+        right_frame = tk.Frame(inner_frame, bg='#202020')
+
+        left_frame.pack(side="left", fill="both", expand=1, padx=10, pady=10)
+        right_frame.pack(side="right", fill="both", expand=1, padx=10, pady=10)
+
+
+class Completion(tk.Frame):
+    
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Completion Screen, we did it!")
+        label.pack(padx=10, pady=10)
+        switch_window_button = ttk.Button(
+            self, text="Return to menu", command=lambda: controller.show_frame(MainPage)
+        )
+        switch_window_button.pack(side="bottom", fill=tk.X)
+
+if __name__ == "__main__":
+    testObj = window()
+    testObj.mainloop()
